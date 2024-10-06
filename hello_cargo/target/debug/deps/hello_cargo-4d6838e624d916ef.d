c4afef0df86c414870aab3e85a1a0f924dde0b37
pizzapizzaa <iamcheesepizzaa@gmail.com>
2024-10-06 14:55:33 +0700

    update typescript

---
+++ hello_cargo/target/debug/deps/hello_cargo-4d6838e624d916ef.d
@@ -0,0 +1,5 @@
+/Users/oliviapc/Desktop/Merch_code/merch/hello_cargo/target/debug/deps/libhello_cargo-4d6838e624d916ef.rmeta: src/main.rs
+
+/Users/oliviapc/Desktop/Merch_code/merch/hello_cargo/target/debug/deps/hello_cargo-4d6838e624d916ef.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-e2c7fff5fab3cf71.d
@@ -0,0 +1,5 @@
+/Users/oliviapc/Desktop/Merch_code/merch/hello_cargo/target/debug/deps/libhello_cargo-e2c7fff5fab3cf71.rmeta: src/main.rs
+
+/Users/oliviapc/Desktop/Merch_code/merch/hello_cargo/target/debug/deps/hello_cargo-e2c7fff5fab3cf71.d: src/main.rs
+
+src/main.rs:
