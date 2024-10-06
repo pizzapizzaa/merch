9f858afc075f6e7636ba2825774eaf51d7171bb2
pizzapizzaa <iamcheesepizzaa@gmail.com>
2024-10-06 23:16:22 +0700

    test laptop

---
+++ hello_cargo/target/debug/deps/hello_cargo-727a8b0310df2d33.d
@@ -0,0 +1,5 @@
+C:\Users\Pizza\Desktop\Merch_code\merch\hello_cargo\target\debug\deps\libhello_cargo-727a8b0310df2d33.rmeta: src\main.rs
+
+C:\Users\Pizza\Desktop\Merch_code\merch\hello_cargo\target\debug\deps\hello_cargo-727a8b0310df2d33.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-800479f91b6650af.d
@@ -0,0 +1,5 @@
+C:\Users\Pizza\Desktop\Merch_code\merch\hello_cargo\target\debug\deps\libhello_cargo-800479f91b6650af.rmeta: src\main.rs
+
+C:\Users\Pizza\Desktop\Merch_code\merch\hello_cargo\target\debug\deps\hello_cargo-800479f91b6650af.d: src\main.rs
+
+src\main.rs:
