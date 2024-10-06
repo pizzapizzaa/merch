33d54bd804ed34e8502ccb30369cfee485af4f33
pizzapizzaa <iamcheesepizzaa@gmail.com>
2024-10-06 11:40:46 +0700

    added rust and cargo files

---
+++ hello_cargo/target/debug/deps/hello_cargo-1c3ab2dc80a674b5.d
@@ -0,0 +1,5 @@
+/Users/oliviapc/Desktop/Merch_code/merch/hello_cargo/target/debug/deps/hello_cargo-1c3ab2dc80a674b5: src/main.rs
+
+/Users/oliviapc/Desktop/Merch_code/merch/hello_cargo/target/debug/deps/hello_cargo-1c3ab2dc80a674b5.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/oliviapc/Desktop/Merch_code/merch/hello_cargo/target/debug/hello_cargo: /Users/oliviapc/Desktop/Merch_code/merch/hello_cargo/src/main.rs
